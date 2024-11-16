1b7cf418ce46fd40f66d550bf770849ba100a077
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-11-17 01:02:36 +0300

    start & dependencies

---
+++ mini-library/src/main/java/com/abdishakoorx/mini_library/MiniLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdishakoorx.mini_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLibraryApiApplication.class, args);
+	}
+
+}
---
+++ mini-library/src/test/java/com/abdishakoorx/mini_library/MiniLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdishakoorx.mini_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
